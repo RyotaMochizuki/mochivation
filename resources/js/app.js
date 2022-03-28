2a8f997c814dbbf2ec67381820f0616dddf2623a
EC2 Default User <ec2-user@ip-172-31-47-118.ap-northeast-1.compute.internal>
2022-03-28 08:16:26 +0000

    サーバープッシュ

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-require('./bootstrap');
-=======
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
- import Vue from 'vue';
- import BootstrapVue from "bootstrap-vue"　// ★追加
- import 'bootstrap/dist/css/bootstrap.css'　// ★追加
- import 'bootstrap-vue/dist/bootstrap-vue.css'　// ★追加
- import VModal from 'vue-js-modal'
- 
- Vue.use(VModal)
- import StudentlayoutComponent from "./components/LayoutComponent";
- import router from './router'
 
 require('./bootstrap');
 
@@ -26,17 +14,12 @@ window.Vue = require('vue').default;
  * components and automatically register them with their "basename".
  *
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- * 
  */
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
- Vue.use(BootstrapVue)
- //Vue.use(IconsPlugin)
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('studentlayout-component', StudentlayoutComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -46,7 +29,4 @@ Vue.component('studentlayout-component', StudentlayoutComponent);
 
 const app = new Vue({
     el: '#app',
-    router,
-    BootstrapVue,
 });
->>>>>>> 54d70f903eae386f658222ced14cb5862c74934f
